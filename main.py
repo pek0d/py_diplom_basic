aabe26a17b371acee5ebb957e5a34a11175a97fe
pekod <onathema@gmail.com>
2023-10-20 16:32:10 +0300

    объединил получение ссылок на фото в одну функцию с получением информации

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from configuration import VK_TOKEN
 import requests  # type: ignore
 import pprint
+import json
 
 pp = pprint.PrettyPrinter(indent=2)
 
@@ -21,7 +22,7 @@ class VKAPIclient:
     def common_params(self):
         return {"access_token": self.access_token, "v": "5.154"}
 
-    def user_info(self):
+    def get_profile_info(self):
         user_url = f"{self.api_base_url}/users.get"
         user_id_input = input(
             "Введите id пользователя VK или его screen_name: ")
@@ -43,7 +44,7 @@ class VKAPIclient:
             self.user_id = response.json()["response"][0]["id"]
         return self.user_id
 
-    def get_profile_photos(self):
+    def get_profile_photos_data(self):
         profile_id = self.user_id
         count_photos = input(
             "Сколько фото из профиля хотите сохранить?: (По умолчанию 5)"
@@ -54,7 +55,7 @@ class VKAPIclient:
             count_photos = int(count_photos)
         except ValueError:
             print("Некорректное значение, нужно ввести число. Попробуйте еще раз")
-            return self.get_profile_photos()
+            return self.get_profile_photos_data()
         params = self.common_params()
         params.update(
             {
@@ -66,9 +67,10 @@ class VKAPIclient:
             }
         )
         response = requests.get(self._build_url("photos.get"), params=params)
-        # with open("get_photos.json", "w") as f:
-        #     json.dump(response.json(), f, indent=2)
-        self.photos_data = response.json()
+        with open("get_photos.json", "w") as f:
+            json.dump(response.json(), f, indent=2)
+        photos_data = response.json()
+        self.photos_data = photos_data
         return self.photos_data
 
     def get_photos_info(self):
@@ -78,25 +80,37 @@ class VKAPIclient:
         info_dict = {}
         for item in items_response:
             likes = item["likes"]["count"]
-            info_dict = {"size": item["sizes"][-5]["type"]}
+            info_dict["size"] = item["sizes"][0]["type"]
             info_dict["file_name"] = f"{likes}.jpg"
             photos_info_lst.append(info_dict)
-        return photos_info_lst
+        self.photos_info_lst = photos_info_lst
 
-    def get_photos_urls(self):
-        profile_photos = self.get_profile_photos()
         target_photo_size = "z"
         photos_urls_lst = []
-        for photo in profile_photos["response"]["items"]:
+        for photo in items_response:
             for size in photo["sizes"]:
                 if size["type"] == target_photo_size:
                     photos_urls_lst.append(size["url"])
+        self.photos_urls_lst = photos_urls_lst
         pp.pprint(photos_urls_lst)
-        return photos_urls_lst
+
+        return self.photos_info_lst, self.photos_urls_lst
+
+    # def get_photos_urls(self):
+    #     profile_photos = self.get_profile_photos_data()
+    #     target_photo_size = "z"
+    #     photos_urls_lst = []
+    #     for photo in profile_photos["response"]["items"]:
+    #         for size in photo["sizes"]:
+    #             if size["type"] == target_photo_size:
+    #                 photos_urls_lst.append(size["url"])
+    #     pp.pprint(photos_urls_lst)
+    #     return photos_urls_lst
+    #
 
 
 if __name__ == "__main__":
     vk_client = VKAPIclient(token, 827020295)
-    vk_client.user_info()
+    vk_client.get_profile_info()
+    vk_client.get_profile_photos_data()
     vk_client.get_photos_info()
-    # vk_client.get_photos_urls()
